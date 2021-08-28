139a9955b6a7be267083d9d8835cb89bde61128d
AlexanderPro <Illarionov.pro@gmail.com>
2021-08-28 11:52:35 +0300

    Added WindowTextExtractor v1.8.3

---
+++ windowtextextractor/tools/chocolateyinstall.ps1
@@ -0,0 +1,25 @@
+﻿$ErrorActionPreference = 'Stop';
+$packageName= 'windowtextextractor'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://github.com/AlexanderPro/WindowTextExtractor/releases/download/v1.8.3/WindowTextExtractor_v1.8.3.zip'
+
+$packageArgs = @{
+  packageName   = $packageName
+  destination   = $toolsDir
+  fileType      = 'zip'
+  url           = $url
+  softwareName  = 'WindowTextExtractor*'
+  checksum      = '9995a1d1b63f3d753c5c7de349eeb0593bf338b52fe0a661a9bbf9fa7140d3ae'
+  checksumType  = 'sha256'
+}
+
+Install-ChocolateyZipPackage @packageArgs
+
+
+
+
+
+
+
+
+
